e9876fdc543bf1f8cea73851ddd41c9d7e2d83e9
Tamas Flesch <tamas.flesch@gmail.com>
2024-04-30 23:35:05 +0200

    change example message

--- src/main/java/hu/saborsoft/blockchain/Main.java
+++ src/main/java/hu/saborsoft/blockchain/Main.java
@@ -5,7 +5,7 @@ import hu.saborsoft.blockchain.support.UtilityMethods;
 public class Main {
 
     public static void main(String[] args) {
-        String msg = "If you are a drop of tears in my eyes!";
+        String msg = "If you are a drop of tears in my eyes";
         String hash = UtilityMethods.messageDigestSHA256_toString(msg);
         System.out.println(hash);
     }
