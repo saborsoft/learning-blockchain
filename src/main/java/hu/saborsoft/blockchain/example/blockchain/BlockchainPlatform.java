966bc80deefc61d069a0a3540a6c9aa3141ee7ed
Flesch Tamás <tamas.flesch@gmail.com>
2024-05-08 09:10:39 +0200

    add more transaction into a new block

--- src/main/java/hu/saborsoft/blockchain/example/blockchain/BlockchainPlatform.java
+++ src/main/java/hu/saborsoft/blockchain/example/blockchain/BlockchainPlatform.java
@@ -18,7 +18,7 @@ public class BlockchainPlatform {
 
     // use this variable to track how much transaction fee has been paid
     // currently no transaction fee has been collected by miners
-    // private static double transactionFee = 0.0;
+     private static double transactionFee = 0.0;
 
     public static void main(String[] args) throws Exception {
         // set the mining difficulty level. 20 is good for practice
@@ -128,9 +128,10 @@ public class BlockchainPlatform {
             if (t1.verifySignature() && b2.addTransaction(t1)) {
                 // display the balance to show that everything works. at this
                 // moment, A, B, C, should have zero balance
-                System.out.println("t1 added to block b2. before b2 is mined "
-                        + "and added to the chain, the balances are:");
-                displayBalanceAfterBlock(genesisBlock, genesisMiner, A, B, C);
+                System.out.println("t1 added to block b2.");
+//                System.out.println("t1 added to block b2. before b2 is mined "
+//                        + "and added to the chain, the balances are:");
+                //displayBalanceAfterBlock(genesisBlock, genesisMiner, A, B, C);
             } else {
                 System.out.println("t1 failed to add to b2");
             }
@@ -145,9 +146,10 @@ public class BlockchainPlatform {
         Transaction t2 = genesisMiner.transferFund(B.getPublicKey(), 200);
         if (t2 != null) {
             if (t2.verifySignature() && b2.addTransaction(t2)) {
-                System.out.println("t2 added to block b2. before b2 is mined "
-                        + "and added to the chain, the balances are:");
-                displayBalanceAfterBlock(genesisBlock, genesisMiner, A, B, C);
+                System.out.println("t2 added to block b2.");
+//                System.out.println("t2 added to block b2. before b2 is mined "
+//                        + "and added to the chain, the balances are:");
+                //displayBalanceAfterBlock(genesisBlock, genesisMiner, A, B, C);
             } else {
                 System.out.println("t2 failed to add to b2");
             }
@@ -167,18 +169,103 @@ public class BlockchainPlatform {
             System.out.println("After block b2 is added to the chain, the balances are:");
             displayBalanceAfterBlock(b2, genesisMiner, A, B, C);
         }
+
+        //////////////////////////////////////////////////////////////////////////////////////
+        // create another block (with some transactions)
+        //////////////////////////////////////////////////////////////////////////////////////
+
+        // another block
+        Block b3 = new Block(blockchain.getLastBlock().getHashID(), difficultyLevel);
+        System.out.println("\n\nBlock b3 created");
+
+        ////////////////////////
+        // t3 transaction
+        ////////////////////////
+
+        // t3 should fail as A does not have enough fund
+        Transaction t3 = A.transferFund(B.getPublicKey(), 200.0);
+        if (t3 != null) {
+            if (t3.verifySignature() && b3.addTransaction(t3)) {
+                System.out.println("t3 added to block b3");
+            } else {
+                System.out.println("t3 failed to add to b3");
+            }
+        } else {
+            System.out.println("t3 failed to create");
+        }
+
+        ////////////////////////
+        // t4 transaction
+        ////////////////////////
+
+        // t4 should fail as A does not have enough fund
+        Transaction t4 = A.transferFund(C.getPublicKey(), 300.0);
+        if (t4 != null) {
+            if (t4.verifySignature() && b3.addTransaction(t4)) {
+                System.out.println("t4 added to block b3");
+            } else {
+                System.out.println("t4 failed to add to b3");
+            }
+        } else {
+            System.out.println("t4 failed to create");
+        }
+
+        ////////////////////////
+        // t5 transaction
+        ////////////////////////
+
+        Transaction t5 = A.transferFund(C.getPublicKey(), 20.0);
+        if (t5 != null) {
+            if (t5.verifySignature() && b3.addTransaction(t5)) {
+                System.out.println("t5 added to block b3");
+            } else {
+                System.out.println("t5 failed to add to b3");
+            }
+        } else {
+            System.out.println("t5 failed to create");
+        }
+
+        ////////////////////////
+        // t6 transaction
+        ////////////////////////
+
+        Transaction t6 = B.transferFund(C.getPublicKey(), 80.0);
+        if (t6 != null) {
+            if (t6.verifySignature() && b3.addTransaction(t6)) {
+                System.out.println("t6 added to block b3");
+            } else {
+                System.out.println("t6 failed to add to b3");
+            }
+        } else {
+            System.out.println("t6 failed to create");
+        }
+
+        ////////////////////////
+        // Mine the third block
+        ////////////////////////
+
+        // mine the block 3.
+        if (C.mineBlock(b3)) {
+            System.out.println("C mined b3, hashID is:");
+            System.out.println(b3.getHashID());
+            blockchain.addBlock(b3);
+            System.out.println("After block b3 is added to the chain, the balances are:");
+            displayBalanceAfterBlock(b3, genesisMiner, A, B, C);
+        }
+
+        System.out.println("==============BlockChain platform shuts down==============");
+
     }
 
     // A method to display the balance of the wallets and miners
-    private static void displayBalanceAfterBlock(Block b, Wallet genesisMiner,
+    private static void displayBalanceAfterBlock(Block b, Wallet wallet,
                                                  Wallet A, Wallet B, Wallet C) {
-        double transactionFee = 0.0;
-        double total = genesisMiner.getCurrentBalance(blockchain)
+        double total = wallet.getCurrentBalance(blockchain)
                 + A.getCurrentBalance(blockchain) + B.getCurrentBalance(blockchain)
                 + C.getCurrentBalance(blockchain);
         transactionFee += b.getTotalNumberOfTransactions() * Transaction.TRANSACTION_FEE;
         System.out.println("genesisMiner="
-                + genesisMiner.getCurrentBalance(blockchain) + ", A="
+                + wallet.getCurrentBalance(blockchain) + ", A="
                 + A.getCurrentBalance(blockchain) + ", B="
                 + B.getCurrentBalance(blockchain) + ", C="
                 + C.getCurrentBalance(blockchain) + ", total cash="
